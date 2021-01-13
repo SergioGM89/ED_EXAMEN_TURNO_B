24415c6f8e830053c7d20ba7551f4012602ab2f4
Sergio Gil <serginho_gil@hotmail.com>
2021-01-13 20:09:35 +0100

    Código sin Javadoc DNI 44878084R

---
+++ src/SergioGil_44878084R/SergioGil_44878084R.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SergioGil_44878084R;
+
+/**
+ *
+ * @author sergio
+ */
+import java.util.*;
+
+public class SergioGil_44878084R {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public SergioGil_44878084R(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
+        SergioGil_44878084R emp = new SergioGil_44878084R("SERGIO Gil", "44878084R");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto 
+		+ " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+
+}
